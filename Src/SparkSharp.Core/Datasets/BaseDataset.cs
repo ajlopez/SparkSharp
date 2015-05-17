bbbd4266c259a305a7d62b0948eea4bdfc423e79
ajlopez <webmaster@ajlopez.com>
2015-05-17 17:27:13 -0300

    BaseDataset refactor

---
+++ Src/SparkSharp.Core/Datasets/BaseDataset.cs
@@ -0,0 +1,39 @@
+﻿namespace SparkSharp.Core.Datasets
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public abstract class BaseDataset<T> : IEnumerable<T>
+    {
+        public EnumDataset<S> Map<S>(Func<T, S> map)
+        {
+            return new EnumDataset<S>(this.ApplyMap(map));
+        }
+
+        public T Reduce(Func<T, T, T> reduce)
+        {
+            T result = default(T);
+
+            foreach (var elem in this)
+                result = reduce(elem, result);
+
+            return result;
+        }
+
+        private IEnumerable<S> ApplyMap<S>(Func<T, S> map)
+        {
+            foreach (var elem in this)
+                yield return map(elem);
+        }
+
+        public abstract IEnumerator<T> GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Src/SparkSharp.Core/Datasets/EnumDataset.cs
+++ Src/SparkSharp.Core/Datasets/EnumDataset.cs
@@ -6,7 +6,7 @@
     using System.Linq;
     using System.Text;
 
-    public class EnumDataset<T> : IEnumerable<T>
+    public class EnumDataset<T> : BaseDataset<T>
     {
         private IEnumerable<T> elements;
 
@@ -15,35 +15,9 @@
             this.elements = elements;
         }
 
-        public EnumDataset<S> Map<S>(Func<T, S> map)
-        {
-            return new EnumDataset<S>(this.ApplyMap(map));
-        }
-
-        public T Reduce(Func<T, T, T> reduce)
-        {
-            T result = default(T);
-
-            foreach (var elem in this)
-                result = reduce(elem, result);
-
-            return result;
-        }
-
-        public IEnumerator<T> GetEnumerator()
+        public override IEnumerator<T> GetEnumerator()
         {
             return this.elements.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return this.elements.GetEnumerator();
-        }
-
-        private IEnumerable<S> ApplyMap<S>(Func<T, S> map)
-        {
-            foreach (var elem in this)
-                yield return map(elem);
         }
     }
 }
