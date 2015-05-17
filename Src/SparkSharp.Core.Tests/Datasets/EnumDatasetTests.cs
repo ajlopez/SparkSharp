934f6b34a5067e5baaa32c87502c24d1fac13cd5
ajlopez <webmaster@ajlopez.com>
2015-05-17 16:49:09 -0300

    EnumDataset get elements

---
+++ Src/SparkSharp.Core.Tests/Datasets/EnumDatasetTests.cs
@@ -0,0 +1,25 @@
+﻿namespace SparkSharp.Core.Tests.Datasets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SparkSharp.Core.Datasets;
+
+    [TestClass]
+    public class EnumDatasetTests
+    {
+        [TestMethod]
+        public void GetElements()
+        {
+            EnumDataset<int> ds = new EnumDataset<int>(new int[] { 1, 2, 3 });
+            var enumerator = ds.GetEnumerator();
+
+            for (int k = 1; enumerator.MoveNext(); k++)
+                Assert.AreEqual(k, enumerator.Current);
+
+            Assert.AreEqual(3, ds.Count());
+        }
+    }
+}
---
+++ Src/SparkSharp.Core/Datasets/EnumDataset.cs
@@ -0,0 +1,28 @@
+﻿namespace SparkSharp.Core.Datasets
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class EnumDataset<T> : IEnumerable<T>
+    {
+        private IEnumerable<T> elements;
+
+        public EnumDataset(IEnumerable<T> elements) 
+        {
+            this.elements = elements;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.elements.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.elements.GetEnumerator();
+        }
+    }
+}
